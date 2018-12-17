2526ff1e216328c3bd8abc4ea6145a93ca7c7499
xoellb@inditex.com <xoellb@inditex.com>
2018-12-17 17:00:20 +0100

    first commit

---
+++ Section 2 JS fundamentals/ex1/ex2/app.js
@@ -0,0 +1,10 @@
+let urlLocation = window.location;
+
+console.log(urlLocation.host);
+console.log(urlLocation.hostname);
+console.log(urlLocation.port);
+console.log(urlLocation.search);
+
+// window.location.href = 'http://floppening.gold';
+
+console.log(urlLocation);
